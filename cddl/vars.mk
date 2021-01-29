517d186fa12ff00cf678aadf7f0d5fa9f6e85491
Henk Birkholz <henk.birkholz@sit.fraunhofer.de>
2021-01-29 18:04:06 +0100

    no signed CoSWIDs or CoMIDs removed signed-coswid.cddl and dependecies

--- cddl/vars.mk
+++ cddl/vars.mk
@@ -2,7 +2,6 @@ CDDL_FRAGS := concise-rim.cddl
 CDDL_FRAGS += signed-corim.cddl
 CDDL_FRAGS += unsigned-corim.cddl
 CDDL_FRAGS += concise-mid-tag.cddl
-CDDL_FRAGS += signed-coswid.cddl
 CDDL_FRAGS += concise-swid-tag.cddl
 
 CDDL_FULL := corim.cddl
